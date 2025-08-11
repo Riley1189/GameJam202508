04298aa6b4fdd339e216fac0dbee852910df992d
Riley <106734393+Riley1189@users.noreply.github.com>
2025-08-11 14:07:10 +1000

    update dice picture

--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -1,18 +1,11 @@
-extends Node2D
-
-
-@export var dice_roll: AnimatedSprite2D
+extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	dice_roll
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func one_round() -> void:
-	pass # Replace with function body.
