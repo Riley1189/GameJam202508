4e47a5e2e54041c31ba3f9085347301d4ba4a91b
Riley <106734393+Riley1189@users.noreply.github.com>
2025-08-11 16:40:58 +1000

    add dice

--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
+#@export var 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(4).timeout
 	queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _roll_number(num: int) ->void:
+	$AnimatedSprite2D.play("roll" + str(num))
+
+	
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -7,10 +7,18 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 #这里的处理逻辑也是同每轮一样，
 #1. 生成骰子，角色被告知新的一轮，以及获得tool+随机属性）
-#2. 骰子销毁，角色开始可以移动开始战斗
+#2. 骰子销毁（骰子自带的3s消失的属性），角色开始可以移动开始战斗
 func _ready() -> void:
-	var dice_node=dice_roll.instantiate()
-	dice_node.position=Vector2(0,0)
+	randomize() #使得每次的随机都不一样
+	#var dice_node1=dice_roll.instantiate()
+	#dice_node1.position=Vector2(-5,0)
+	var dice_node_tools1=dice_roll.instantiate()
+	dice_node_tools1.position=Vector2(-5,0)
+	get_tree().current_scene.add_child(dice_node_tools1)
+	
+	await get_tree().create_timer(2).timeout
+	var tools_number1=randi_range(1,6)
+	dice_node_tools1._roll_number(tools_number1)
 	
 
 
@@ -24,6 +32,7 @@ func _process(delta: float) -> void:
 #2. 骰子销毁，（角色开始可以移动开始战斗
 func one_round() -> void:
 	$player1.is_new_round=true
-	var dice_node=dice_roll.instantiate()
-	dice_node.position=Vector2(0,0)
+	var dice_node_tools=dice_roll.instantiate()
+	dice_node_tools.position=Vector2(0,0)
+	dice_node_tools._roll_number(randi_range(1,6))
 	#pass # Replace with function body.
